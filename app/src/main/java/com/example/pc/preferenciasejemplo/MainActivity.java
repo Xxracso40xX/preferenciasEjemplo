2becb65788a77fa184ba090635e337bba677f857
Óscar <Xxracso40xX@gmail.com>
2019-01-16 10:07:02 +0100

    Ejercicio12 tema 6

--- app/src/main/java/com/example/pc/preferenciasejemplo/MainActivity.java
+++ app/src/main/java/com/example/pc/preferenciasejemplo/MainActivity.java
@@ -4,15 +4,41 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button boton;
+    TextView txt;
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int edad=10;
+        boton = findViewById(R.id.button);
+        txt = findViewById(R.id.editText);
+        prefs = getSharedPreferences("ficheroconfiguracion", Context.MODE_PRIVATE);
+        int variableTelefono = prefs.getInt("telefono", 0);
+        if(variableTelefono !=0)
+        {
+            txt.setText(""+variableTelefono);
+        }
+        else
+        {
+
+        }
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putInt("telefono", Integer.parseInt(txt.getText().toString()));
+                editor.commit();
+            }
+        });
+       // int edad=10;
        /*SharedPreferences prefs = getSharedPreferences("ficheroconfiguracion", Context.MODE_PRIVATE);
          SharedPreferences.Editor editor = prefs.edit();
         editor.putInt("edadUsuario", edad);
